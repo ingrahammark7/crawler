95a2ee570a4c88260349fdbe95246955f9675c24
ingrahammark7 <ingrahammark7@gmail.com>
2022-03-18 02:45:37 -0700

    Add files via upload

---
+++ tool/src/main/java/tools/tool/App.java
@@ -0,0 +1,7 @@
+package tools.tool;
+
+public class App {
+	public static void main(String[] args) throws Exception {
+		takefile.dofile();
+	}
+}
---
+++ tool/src/main/java/tools/tool/takefile.java
@@ -0,0 +1,55 @@
+package tools.tool;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
+
+public class takefile {
+
+	public static HashMap<String, String> vals = new HashMap<String, String>();
+
+	public static void dofile() throws Exception {
+		new takefile().getfile();
+	}
+
+	public void getfile() throws Exception {
+
+		Class clazz = takefile.class;
+		InputStream inputStream = clazz.getResourceAsStream("/templates/c.txt");
+		String s = readFromInputStream(inputStream);
+		String[] ss = s.split("/", 0);
+		String one = ss[0].split("\t")[0];
+		String two = ss[0].split("\t")[1];
+		vals.put(one, two);
+		for (int i = 1; i < ss.length; ++i) {
+			String[] arr = ss[i].split("\t");
+			if (arr.length > 10)
+				for (int j = 0; j < arr.length; ++j) {
+					vals.put(arr[9], arr[10]);
+				}
+		}
+		Set<String> keys = vals.keySet();
+		Iterator<String> it = keys.iterator();
+		while (it.hasNext()) {
+			String s1 = it.next();
+			String s2 = vals.get(s1);
+			System.out.println("key " + s1 + " value " + s2);
+		}
+	}
+
+	private String readFromInputStream(InputStream inputStream) throws IOException {
+		StringBuilder resultStringBuilder = new StringBuilder();
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
+			String line;
+			while ((line = br.readLine()) != null) {
+				resultStringBuilder.append(line).append("\n");
+			}
+		}
+		return resultStringBuilder.toString();
+	}
+
+}
