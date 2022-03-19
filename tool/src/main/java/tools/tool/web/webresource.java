5d1af5a7c62aa01f6a151e580322f37ceb452a5d
ingrahammark7 <ingrahammark7@gmail.com>
2022-03-19 05:48:25 -0700

    Add files via upload

--- tool/src/main/java/tools/tool/web/webresource.java
+++ tool/src/main/java/tools/tool/web/webresource.java
@@ -20,12 +20,47 @@ public class webresource {
 			String count = links.get(t);
 			dosublinks(t, count);
 		}
-
 	}
 
-	public static void dosublinks(String t, String count) {
-		System.out.println(t);
-		System.out.println(count);
+	public static void dosublinks(String t, String count) throws Exception {
+		String r = request(t);
+		count = count.replace(" ", "");
+		int pages = (Integer.parseInt(count)) / 10;
+		pages = pages + 1;
+		for (int i = 0; i < pages; ++i) {
+			int bar = i + 1;
+			String tempr = r + "?page=" + bar + "#Review";
+			HashMap<String, String> test = getsublink(tempr);
+			Iterator it = test.keySet().iterator();
+			while (it.hasNext()) {
+				String foo = (String) it.next();
+				foo = request(foo);
+				System.out.println("final result " + foo);
+			}
+		}
+	}
+
+	public static HashMap<String, String> getsublink(String r) {
+		String[] links = r.split("/reviews/");
+		HashMap<String, String> map = new HashMap<String, String>();
+		for (int i = 1; i < links.length; ++i) {
+			String t = links[i];
+			String[] t1 = t.split("\"");
+			t = t1[0];
+			if (t.contains(".asp") || t.contains("city")) {
+				continue;
+			}
+			for (int i1 = 0; i1 < t1.length; ++i1) {
+				String temp = t1[i1];
+				if (temp.contains("detail")) {
+					map.put("/reviews/" + t, "");
+				} else {
+
+				}
+			}
+
+		}
+		return map;
 	}
 
 	public static HashMap<String, String> getlink(String r) {
@@ -57,13 +92,12 @@ public class webresource {
 		String site2 = url;
 		HttpURLConnection conn2 = (HttpURLConnection) new URL(site + site2).openConnection();
 		conn2.setRequestProperty("cookie",
-				"MPA3=1; _ga=GA1.2.136954141.1647589240; _gid=GA1.2.163485560.1647589240; terAgreementVer=1; TERLocationResolved=1; TerTzOffset=-420; TER%5FtestCookie=; language=en; TER%5FRememberPW=1; MsTerVer=6; cookieconsent_status=dismiss; TER%5Fusername=mingraham; TER%5Fhash=8OdI913oxsEJLWRRLLYHQSUKneB8L6vGp%2Fa8SadMjdx4QzatOvcGQJSnWZs714eWQcwwvCwEn%2BaSiXouCSMPzg%3D%3D; tz=Mountain+Standard+Time; TER%5FSearchGeoCityName=Los+Angeles%2C+CA; TER%5FSearchGeoCityId=city%2Dlos%2Dangeles%2Dca%2Dus; Upgraded=1; MsTerCounter=6; GUID=C1400F12BA24A549864A9BF56D86F951; K-GUID-pocpghie=09A0F0973C6C38D4117552EC1DCD4E51; TER%5Fsplash=1");
+				"MPA3=1; _ga=GA1.2.136954141.1647589240; _gid=GA1.2.163485560.1647589240; terAgreementVer=1; TERLocationResolved=1; TerTzOffset=-420; TER%5FtestCookie=; language=en; TER%5FRememberPW=1; MsTerVer=6; cookieconsent_status=dismiss; TER%5Fusername=mingraham; TER%5Fhash=8OdI913oxsEJLWRRLLYHQSUKneB8L6vGp%2Fa8SadMjdx4QzatOvcGQJSnWZs714eWQcwwvCwEn%2BaSiXouCSMPzg%3D%3D; tz=Mountain+Standard+Time; TER%5FSearchGeoCityName=Los+Angeles%2C+CA; TER%5FSearchGeoCityId=city%2Dlos%2Dangeles%2Dca%2Dus; Upgraded=1; MsTerCounter=6; K-GUID-pocpghie=09A0F0973C6C38D4117552EC1DCD4E51; TER%5Fsplash=1; GUID=438BB5B3648691499A7729D7FAA29DE1; _gat=1");
 		return doreq(conn2);
 	}
 
 	public static String doreq(HttpURLConnection con) throws Exception {
 		int responseCode = con.getResponseCode();
-		System.out.println("GET Response Code :: " + responseCode);
 		if (responseCode == HttpURLConnection.HTTP_OK) { // success
 			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
 			String inputLine;
@@ -74,7 +108,7 @@ public class webresource {
 			in.close();
 			return response.toString();
 		} else {
-			System.out.println("GET request not worked");
+			System.out.println("failed request");
 		}
 		return "";
 
